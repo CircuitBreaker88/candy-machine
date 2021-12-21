0ac223fd257a7b624ed73399df73e4f2e66464af
CircuitBreaker88 <sarris88p@gmail.com>
2021-12-21 14:36:07 -0500

    Candy-Machine Base

---
+++ src/Home.tsx
@@ -0,0 +1,278 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Countdown from "react-countdown";
+import { Button, CircularProgress, Snackbar } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+import './index.tsx';
+
+
+import * as anchor from "@project-serum/anchor";
+
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
+import { WalletDialogButton } from "@solana/wallet-adapter-material-ui";
+
+import {
+  CandyMachine,
+  awaitTransactionSignatureConfirmation,
+  getCandyMachineState,
+  mintOneToken,
+  shortenAddress,
+} from "./candy-machine";
+
+
+const ConnectButton = styled(WalletDialogButton)``;
+
+const CounterText = styled.span`
+text-align: center;
+`; // add your styles here
+
+const MintContainer = styled.div`
+text-align: center;
+`; // add your styles here
+
+const MintButton = styled(Button)`
+
+background: -webkit-linear-gradient(to right, #fc00ff, #00dbde, #7b4397);
+background: linear-gradient(to right, #fc00ff, #00dbde, #7b4397);
+box-shadow: 0 10px 10px -2px rgba(0,0,0,.25);
+text-align: center;
+
+
+   
+   .btn-grad {background-image: linear-gradient(to right, #B2FEFA 0%, #0ED2F7  51%, #B2FEFA  100%)}
+
+
+`; // add your styles here
+
+export interface HomeProps {
+  candyMachineId: anchor.web3.PublicKey;
+  config: anchor.web3.PublicKey;
+  connection: anchor.web3.Connection;
+  startDate: number;
+  treasury: anchor.web3.PublicKey;
+  txTimeout: number;
+}
+
+const Home = (props: HomeProps) => {
+  const [balance, setBalance] = useState<number>();
+  const [isActive, setIsActive] = useState(false); // true when countdown completes
+  const [isSoldOut, setIsSoldOut] = useState(false); // true when items remaining is zero
+  const [isMinting, setIsMinting] = useState(false); // true when user got to press MINT
+
+  const [itemsAvailable, setItemsAvailable] = useState(0);
+  const [itemsRedeemed, setItemsRedeemed] = useState(0);
+  const [itemsRemaining, setItemsRemaining] = useState(0);
+
+  const [alertState, setAlertState] = useState<AlertState>({
+    open: false,
+    message: "",
+    severity: undefined,
+  });
+
+  const [startDate, setStartDate] = useState(new Date(props.startDate));
+
+  const wallet = useAnchorWallet();
+  const [candyMachine, setCandyMachine] = useState<CandyMachine>();
+
+  const refreshCandyMachineState = () => {
+    (async () => {
+      if (!wallet) return;
+
+      const {
+        candyMachine,
+        goLiveDate,
+        itemsAvailable,
+        itemsRemaining,
+        itemsRedeemed,
+      } = await getCandyMachineState(
+        wallet as anchor.Wallet,
+        props.candyMachineId,
+        props.connection
+      );
+
+      setItemsAvailable(itemsAvailable);
+      setItemsRemaining(itemsRemaining);
+      setItemsRedeemed(itemsRedeemed);
+
+      setIsSoldOut(itemsRemaining === 0);
+      setStartDate(goLiveDate);
+      setCandyMachine(candyMachine);
+    })();
+  };
+
+  const onMint = async () => {
+    try {
+      setIsMinting(true);
+      if (wallet && candyMachine?.program) {
+        const mintTxId = await mintOneToken(
+          candyMachine,
+          props.config,
+          wallet.publicKey,
+          props.treasury
+        );
+
+        const status = await awaitTransactionSignatureConfirmation(
+          mintTxId,
+          props.txTimeout,
+          props.connection,
+          "singleGossip",
+          false
+        );
+
+        if (!status?.err) {
+          setAlertState({
+            open: true,
+            message: "Congratulations! Mint succeeded!",
+            severity: "success",
+          });
+        } else {
+          setAlertState({
+            open: true,
+            message: "Mint failed! Please try again!",
+            severity: "error",
+          });
+        }
+      }
+    } catch (error: any) {
+      // TODO: blech:
+      let message = error.msg || "Minting failed! Please try again!";
+      if (!error.msg) {
+        if (error.message.indexOf("0x138")) {
+        } else if (error.message.indexOf("0x137")) {
+          message = `SOLD OUT!`;
+        } else if (error.message.indexOf("0x135")) {
+          message = `Insufficient funds to mint. Please fund your wallet.`;
+        }
+      } else {
+        if (error.code === 311) {
+          message = `SOLD OUT!`;
+          setIsSoldOut(true);
+        } else if (error.code === 312) {
+          message = `Minting period hasn't started yet.`;
+        }
+      }
+
+      setAlertState({
+        open: true,
+        message,
+        severity: "error",
+      });
+    } finally {
+      if (wallet) {
+        const balance = await props.connection.getBalance(wallet.publicKey);
+        setBalance(balance / LAMPORTS_PER_SOL);
+      }
+      setIsMinting(false);
+      refreshCandyMachineState();
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (wallet) {
+        const balance = await props.connection.getBalance(wallet.publicKey);
+        setBalance(balance / LAMPORTS_PER_SOL);
+      }
+    })();
+  }, [wallet, props.connection]);
+
+  useEffect(refreshCandyMachineState, [
+    wallet,
+    props.candyMachineId,
+    props.connection,
+  ]);
+
+  return (
+    <div className="flex justify-center mb-3 bg-100 p-4">
+      <div className="rounded-xl shadow-md bg-blue max-w-sm">
+        <div className="border-gray-300 p-8">
+    <main>
+      <div className="rounded-xl shadow-md bg-cyan max-w-sm font-bold rounded-xl text-center underline text-black pb-0.5">
+      <h1 className="text-2xl">The Dope Pandas</h1>
+      
+      <p className="justify-center text-center text-black underline font-bold pb-2">Mint Cost 0.777</p>
+      </div>
+      {wallet && (
+      <div className="text-center text-black p-3 shadow-md bg-cyan rounded-md">
+      {wallet && (
+        <p className="p-0.5">Wallet {shortenAddress(wallet.publicKey.toBase58() || "")}</p>
+      )}
+
+      {wallet && <p className="p-0.5">Balance: {(balance || 0).toLocaleString()} SOL</p>}
+
+      {wallet && <p className="p-0.5">Total Available: {itemsAvailable}</p>}
+
+      {wallet && <p className="p-0.5">Redeemed: {itemsRedeemed}</p>}
+
+      {wallet && <p className="p-0.5">Remaining: {itemsRemaining}</p>}
+      </div>
+      )}
+
+      <p className="p-1"></p>
+      <div>
+      <MintContainer>
+        {!wallet ? (
+          <div><ConnectButton>Connect Wallet</ConnectButton></div>
+        ) : (            
+          <MintButton
+            disabled={isSoldOut || isMinting || !isActive}
+            onClick={onMint}
+            variant="contained"
+          >
+            {isSoldOut ? (
+              "SOLD OUT"
+            ) : isActive ? (
+              isMinting ? (
+                <CircularProgress />
+              ) : (
+                "MINT"
+              )
+            ) : (
+              <Countdown
+                date={startDate}
+                onMount={({ completed }) => completed && setIsActive(true)}
+                onComplete={() => setIsActive(true)}
+                renderer={renderCounter}
+              />
+            )}
+          </MintButton>
+        )}
+      </MintContainer>
+      </div>
+
+      <Snackbar
+        open={alertState.open}
+        autoHideDuration={6000}
+        onClose={() => setAlertState({ ...alertState, open: false })}
+      >
+        <Alert
+          onClose={() => setAlertState({ ...alertState, open: false })}
+          severity={alertState.severity}
+        >
+          {alertState.message}
+        </Alert>
+      </Snackbar>
+    </main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface AlertState {
+  open: boolean;
+  message: string;
+  severity: "success" | "info" | "warning" | "error" | undefined;
+}
+
+const renderCounter = ({ days, hours, minutes, seconds, completed }: any) => {
+  return (
+    <CounterText>
+      {hours + (days || 0) * 24} hours, {minutes} minutes, {seconds} seconds
+    </CounterText>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    "./src/App.tsx",
+    "./src/Home.tsx",
+    "./src/index.tsx"
+  ],
+  theme:{
+  colors: {
+    'blue': '#0050EC',
+    'green': '#80FF00',
+    'cyan': '#3BFFF9',
+    'black': '#000000',
+    'pink': '#C629A0',
+    'triple': ['#27B0E6', '#FA52A0', ''],
+  },
+},
+  background: {
+    'bg': '#0D0415',
+  },
+  fontFamily: {
+    'permanent': ['Permanent Marker', 'cursive']
+  },
+
+    extend: {
+  },
+  plugins: [],
+}
